2f1bcf82b5022407c813db55f6f898a4ec4d841c
Victor <you@example.com>
2019-09-27 16:06:53 -0300

    finalização

--- a.c
+++ a.c
@@ -59,7 +59,7 @@ int g(int **matriz, int m){
     int i, j,soma=0;
     for (i = 0; i < m; i++){
         for (j = 0; j < i; j++){
-            soma += matriz[i][j];
+            soma += matriz[i][j]*matriz[j][i];
         }
     }
     return soma;
@@ -89,6 +89,21 @@ void main(){
         }
         printf("\n");
     }
+
+    //exercício a
+    printf("\nResposta letra a: %d", a(matriz,m,n));
+    //exercício b
+    printf("\nResposta letra b: %d", b(matriz,m,n));
+    //exercício c
+    printf("\nResposta letra c: %d", c(matriz,m,n));
+    //exercício d
+    printf("\nResposta letra d: %d", d(matriz,m));
+    //exercício e
+    printf("\nResposta letra e: %d", e(matriz,m));
+    //exercício f
+    printf("\nResposta letra f: %d", f(matriz,m,n));
+    //exercício g
+    printf("\nResposta letra g: %d", g(matriz,m));
 
 
     
